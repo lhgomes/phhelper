fceb4a981586c102b85dd3a8525ce73a026bfe47
Gomes <luizgm@8c859043585d.ant.amazon.com>
2020-03-11 18:57:22 -0300

    FEATURE Travis integration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='phhelper',
-    version='0.7',
+    version='0.8',
     author='Luiz Henrique Gomes',
     author_email="lhgnet@gmail.com",
     description='Python Handler Helper for Lambda',
