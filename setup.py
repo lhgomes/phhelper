a15404c2d0a75dcee2fc3e88ff308ac139fce962
Gomes <luizgm@8c859043585d.ant.amazon.com>
2020-03-11 23:02:01 -0300

    PyPI integration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='phhelper',
-    version='0.9',
+    version='0.9.1',
     author='Luiz Henrique Gomes',
     author_email="lhgnet@gmail.com",
     description='Python Handler Helper for Lambda',
