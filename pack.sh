0e8560d324c78b02880a7f379e5f8b3329763aa2
Gomes <luizgm@8c859043585d.ant.amazon.com>
2020-03-12 00:15:16 -0300

    Travis build review

--- pack.sh
+++ pack.sh
@@ -1,5 +1,7 @@
+mkdir -p build/python/lib/python3.7/site-packages
+mkdir -p build/python/lib/python3.8/site-packages
 cp -R phhelper build/python/lib/python3.7/site-packages
 cp -R phhelper build/python/lib/python3.8/site-packages
 cd build
-zip -q -r ../../phhelper.zip python
+zip -q -r ../phhelper.zip python
 cd ..
